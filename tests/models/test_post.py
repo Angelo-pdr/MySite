064c4eb5ed88e481c684cea9e9c747d8e584e752
unknown <angelopedrosa00@gmail.com>
2023-09-30 21:22:58 -0300

    erro no teste

--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -3,8 +3,7 @@ import pytest
 from blog.factories import PostFactory
 
 @pytest.fixture
-
-def post_fixture():
+def post_published():
     return PostFactory(title="pytest with factory")
 
 @pytest.mak.django_db
