6d075cd4b227a30c04ac4bb6016ab0362b188aaf
unknown <angelopedrosa00@gmail.com>
2023-10-08 19:10:52 -0300

    feito exercicio da ebac

--- tests/views/test_post.py
+++ tests/views/test_post.py
@@ -8,8 +8,4 @@ def test_post_view(client):
     url = reverse('home')
     response = client.get(url)
     assert response.status_code == 200
-
-    import pdb; pdb.set_trace()
-    response = json.loads(response.content)
-
-    assert response.content == 'Hello World'
+    assert response.content == b'Hello World'
